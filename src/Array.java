20c81bce64687a14661186c82ae442b197203b6f
Jason Tran <jasontran2022@gmail.com>
2020-07-30 10:21:15 -0400

    fixed style of code and added test folder

--- src/Array.java
+++ src/Array.java
@@ -2,105 +2,116 @@ import java.util.Iterator;
 
 /**
  * A Dynamic Array (resizable) Interface that supports generics
+ * 
  * @author Jason Tran
  * @param <E> the type of the elements in the array
  */
 public interface Array<E> extends Iterable<E> {
-  /**
-   * Returns the size of array (number of elements in Array)
-   * @return the number of elements in the array
-   */
-  public int size();
+    /**
+     * Returns the size of array (number of elements in Array)
+     * 
+     * @return the number of elements in the array
+     */
+    public int size();
 
-  /**
-   * Tells whether the array has elements or not
-   * @return true if empty otherwise false
-   */
-  public boolean isEmpty();
+    /**
+     * Tells whether the array has elements or not
+     * 
+     * @return true if empty otherwise false
+     */
+    public boolean isEmpty();
 
-  /**
-   * Tells whether o is in Array or not
-   * @param o an element to be found in the array
-   * @return true if the array contains o otherwise false
-   */
-  public boolean contains(Object o);
+    /**
+     * Tells whether o is in Array or not
+     * 
+     * @param o an element to be found in the array
+     * @return true if the array contains o otherwise false
+     */
+    public boolean contains(Object o);
 
-  /**
-   * This method returns the index of element o
-   * If there are duplicates return the first index of o
-   * @param o an element to search the index for
-   * @return the index of o or -1 if o is not in Array
-   */
-  public int indexOf(Object o);
+    /**
+     * This method returns the index of element o If there are duplicates return the
+     * first index of o
+     * 
+     * @param o an element to search the index for
+     * @return the index of o or -1 if o is not in Array
+     */
+    public int indexOf(Object o);
 
-  /**
-   * This method gets the specfic element at index
-   * array[index]
-   * @param index the index in the array
-   * @return array[index], the element at that index
-   * @throws IllegalArgumentException if index is out of bounds
-   */
-  public E get(int index);
+    /**
+     * This method gets the specfic element at index array[index]
+     * 
+     * @param index the index in the array
+     * @return array[index], the element at that index
+     * @throws IllegalArgumentException if index is out of bounds
+     */
+    public E get(int index);
 
-  /**
-   * This method changes the element at the specified index
-   * @param index the index in the array
-   * @param e the element to be changed
-   * @throws IllegalArgumentException if index is out of bounds
-   */
-  public void set(int index, E e);
+    /**
+     * This method changes the element at the specified index
+     * 
+     * @param index the index in the array
+     * @param e     the element to be changed
+     * @throws IllegalArgumentException if index is out of bounds
+     */
+    public void set(int index, E e);
 
-  /**
-   * This method appends the element e at the end of this array
-   * @param e the element to be added into the array
-   */
-  public void add(E e);
+    /**
+     * This method appends the element e at the end of this array
+     * 
+     * @param e the element to be added into the array
+     */
+    public void add(E e);
 
-  /**
-   * This method removes the element at the specified index, 
-   * shifts any subsequent elements to the left
-   * @param index the index in the array
-   * @return the element at that index
-   * @throws IllegalArgumentException if index is out of bounds
-   * @throws NoSuchElementExpection if the array is empty
-   */
-  public E remove(int index);
+    /**
+     * This method removes the element at the specified index, shifts any subsequent
+     * elements to the left
+     * 
+     * @param index the index in the array
+     * @return the element at that index
+     * @throws IllegalArgumentException if index is out of bounds
+     * @throws NoSuchElementExpection   if the array is empty
+     */
+    public E remove(int index);
 
-  /**
-   * This method deletes the element o from the array, removes the 
-   * first occurancee of e in the array
-   * @param o an element to be remove
-   * @return true if removal of o is successful otherwise false
-   * @throws NoSuchElementExpection if the array is empty
-   */
-  public boolean remove(Object o);
+    /**
+     * This method deletes the element o from the array, removes the first
+     * occurancee of e in the array
+     * 
+     * @param o an element to be remove
+     * @return true if removal of o is successful otherwise false
+     * @throws NoSuchElementExpection if the array is empty
+     */
+    public boolean remove(Object o);
 
-  /**
-   * This method deletes all the elements in the array, the
-   * array will be empty after this call returns
-   */
-  public void clear();
+    /**
+     * This method deletes all the elements in the array, the array will be empty
+     * after this call returns
+     */
+    public void clear();
 
-  /**
-   * Returns an array containing all of the elements in this list
-   * in proper sequence (from first to last element), must allocate 
-   * a new array
-   * @return an array containing all of the elements in this list
-   * in proper sequence
-   */
-  public Object[] toArray();
+    /**
+     * Returns an array containing all of the elements in this list in proper
+     * sequence (from first to last element), must allocate a new array
+     * 
+     * @return an array containing all of the elements in this list in proper
+     *         sequence
+     */
+    public Object[] toArray();
 
-  /**
-   * Returns an iterator over the elements in this array, ordered from
-   * first to last
-   * @return an iterator over the elements in this array
-   */
-  public Iterator<E> iterator();
+    /**
+     * Returns an iterator over the elements in this array, ordered from first to
+     * last
+     * 
+     * @return an iterator over the elements in this array
+     */
+    public Iterator<E> iterator();
 
-  /**
-   * This method returns a string representation of the whole array 
-   * in the format [a_1, a_2, ...., a_n]
-   * @return the string format of the array
-   */
-  public String toString();
+    /**
+     * This method returns a string representation of the whole array in the format
+     * [a_1, a_2, ...., a_n]
+     * 
+     * @return the string format of the array
+     */
+    public String toString();
 }
--- src/ArrayImpl.java
+++ src/ArrayImpl.java
@@ -3,289 +3,289 @@ import java.util.NoSuchElementException;
 import java.util.ConcurrentModificationException;
 
 /**
- * This class implements the array interface using a resizable generic
- * array, also supports null elements to be insert, search, and delete
- * in the array
+ * This class implements the array interface using a resizable generic array,
+ * also supports null elements to be insert, search, and delete in the array
+ * 
  * @param <E> the type of the elements in the array
  * @author Jason Tran
  */
 @SuppressWarnings("unchecked")
 public class ArrayImpl<E> implements Array<E> {
-  private int size; // the num of elements in arr
-  private E[] arr; // a static generic array
+    private int size; // the number of elements in arr
+    private E[] arr; // a static generic array
 
-  /**
-   * Constructor: Creates a new empty ArrayImpl of default capacity 2
-   */
-  public ArrayImpl() {
-    this(2);
-  }
-
-  /**
-   * Constuctor: Creates a new empty ArrayImpl with a specified capacity
-   * @param length the length of the array
-   * @throws IllegalArgumentException if the size is negative
-   */
-  public ArrayImpl(int length) {
-    if (length < 0) {
-      throw new IllegalArgumentException("ERROR: Illegal length to create array: " + length);
+    /**
+     * Constructor: Creates a new empty ArrayImpl of default capacity 2
+     */
+    public ArrayImpl() {
+        this(2);
     }
-    arr = (E[]) new Object[length];
-  }
 
-  /**
-   * This method returns the actual underlying array
-   * @return arr
-   * Runtime: O(1)
-   */
-  public E[] getArray() {
-    return arr;
-  }
-
-  /**
-   * {@inheritDoc}
-   * Runtime: O(1)
-   */
-  @Override
-  public int size() {
-    return size;
-  }
-
-  /**
-   * {@inheritDoc}
-   * Runtime: O(1)
-   */
-  @Override
-  public boolean isEmpty() {
-    return size == 0;
-  }
-
-  /**
-   * {@inheritDoc}
-   * Runtime: O(n)
-   */
-  @Override
-  public boolean contains(Object o) {
-    return indexOf(o) != -1;
-  }
-
-  /**
-   * {@inheritDoc}
-   * Runtime: O(n)
-   */
-  @Override
-  public int indexOf(Object o) {
-    if (o == null) {
-      for (int i = 0; i < size; i++) {
-        if (arr[i] == null) {
-          return i;
+    /**
+     * Constructor: Creates a new empty ArrayImpl with a specified capacity
+     * @param length the length of the array
+     * @throws IllegalArgumentException if the size is negative
+     */
+    public ArrayImpl(int length) {
+        if (length < 0) {
+            throw new IllegalArgumentException("ERROR: Illegal length to create array: " + length);
         }
-      }
-    } else {
-      for (int i = 0; i < size; i++) {
-        if (arr[i].equals(o)) {
-          return i;
-        }
-      }
+        arr = (E[]) new Object[length];
     }
-    return -1;
-  }
 
-  /**
-   * {@inheritDoc}
-   * Runtime: O(1)
-   */
-  @Override
-  public E get(int index) {
-    // check of index is within the apporiate range
-    if (index < 0 || index >= size) {
-      throw new IllegalArgumentException("ERROR: Illegal Index to get element: " + index);
+    /**
+     * This method returns the actual underlying array
+     * @return arr 
+     * Runtime: O(1)
+     */
+    public E[] getArray() {
+        return arr;
     }
-    return arr[index];
-  }
 
-  /**
-   * {@inheritDoc}
-   * Runtime: O(1)
-   */
-  @Override
-  public void set(int index, E e) {
-    // check of index is within the apporiate range
-    if (index < 0 || index >= size) {
-      throw new IllegalArgumentException("ERROR: Illegal Index to set: " + index);
+    /**
+     * {@inheritDoc} 
+     * Runtime: O(1)
+     */
+    @Override
+    public int size() {
+        return size;
     }
-    arr[index] = e;
-  }
 
-  /**
-   * {@inheritDoc} 
-   * When the size is at capacity, the array will 
-   * resize double its capacity
-   * Runtime: Amorizted O(1), Worst Case O(n)
-   */
-  @Override
-  public void add(E e) {
-    // double the length of the array if size is at capacity
-    if (size == arr.length) {
-      resize(2 * arr.length);
+    /**
+     * {@inheritDoc} 
+     * Runtime: O(1)
+     */
+    @Override
+    public boolean isEmpty() {
+        return size == 0;
     }
-    arr[size++] = e;
-  }
 
-  /**
-   * {@inheritDoc}
-   * When the size is less than a quarter of capcaity, the array
-   * capacity will resize down by half
-   * Runtime: O(n)
-   */
-  @Override
-  public E remove(int index) {
-    if (isEmpty()) {
-      throw new NoSuchElementException("ERROR: can not remove from an empty array");
-    } else if (index < 0 || index >= size) {
-      throw new IllegalArgumentException("ERROR: Illegal Index to remove: " + index);
-    } else {
-      E prev = arr[index];
-      // shift the elements towards the left
-      for (int i = index; i < size; i++) {
-        arr[i] = arr[i + 1];
-      }
-      size--;
-      // resize the array by half is size less than a fourth of capacity
-      if (size < arr.length / 4) {
-        resize(arr.length / 2);
-      }
-      return prev;
+    /**
+     * {@inheritDoc} 
+     * Runtime: O(n)
+     */
+    @Override
+    public boolean contains(Object o) {
+        return indexOf(o) != -1;
     }
-  }
 
-  /**
-   * {@inheritDoc}
-   * Runtime: O(n)
-   */
-  @Override
-  public boolean remove(Object o) {
-    if (isEmpty()) {
-      throw new NoSuchElementException("ERROR: can not remove from an empty array");
-    }
-    if (o == null) {
-      for (int i = 0; i < size; i++) {
-        if (arr[i] == null) {
-          remove(i);
-          return true;
-        }
-      }
-    } else {
-      for (int i = 0; i < size; i++) {
-        if (arr[i].equals(o)) {
-          remove(i);
-          return true;
-        }
-      }
-    }
-    return false;
-  }
-
-  /**
-   * This is a helper method that resizes the 
-   * underyling generic dyanmic array 
-   * @param length the length of the array
-   * Runtime: O(n)
-   */
-  private void resize(int length) {
-    E[] temp = (E[]) new Object[length];
-    for (int i = 0; i < size; i++) {
-      temp[i] = arr[i];
-    }
-    arr = temp;
-  }
-
-  /**
-   * {@inheritDoc}
-   * Runtime: O(1)
-   */
-  @Override
-  public void clear() {
-    arr = (E[]) new Object[2];
-    size = 0;
-  }
-
-  /**
-   * {@inheritDoc}
-   * Runtime: O(n)
-   */
-  @Override
-  public Object[] toArray() {
-    Object[] array = new Object[size];
-    for (int i = 0; i < size; i++) {
-      array[i] = arr[i];
-    }
-    return array;
-  }
-
-  /**
-   * {@inheritDoc}
-   */
-  @Override
-  public Iterator<E> iterator() {
-    return new Iterator<E>() {
-      private int count = 0; // the counter of elements traveled so far
-      final private int expectedSize = size; // check for concurrent modification
-
-      /**
-       * {@inheritDoc}
-       * Checks for concurrent modification
-       * @throws ConcurrentModificationException 
-       */
-      @Override
-      public boolean hasNext() {
-        if (expectedSize != size) {
-          throw new ConcurrentModificationException("ERROR: Cannot modifiy the iterator");
-        }
-        return count < expectedSize;
-      }
-
-      /**
-       * {@inheritDoc}
-       * Checks for concurrent modification
-       * @throws ConcurrentModificationException 
-       */
-      @Override
-      public E next() {
-        if (expectedSize != size) {
-          throw new ConcurrentModificationException("ERROR: Cannot modifiy the iterator");
-        } 
-        if (hasNext()) {
-          return arr[count++];
+    /**
+     * {@inheritDoc} 
+     * Runtime: O(n)
+     */
+    @Override
+    public int indexOf(Object o) {
+        if (o == null) {
+            for (int i = 0; i < size; i++) {
+                if (arr[i] == null) {
+                    return i;
+                }
+            }
         } else {
-          throw new NoSuchElementException("ERROR: No more elements to iterate");
+            for (int i = 0; i < size; i++) {
+                if (arr[i].equals(o)) {
+                    return i;
+                }
+            }
         }
-      }
-
-      /**
-       * {@inheritDoc}
-       */
-      @Override
-      public void remove() {
-        throw new UnsupportedOperationException("ERROR: Remove not Supported by this Iterator");
-      }
-    };
-  }
-
-  /**
-   * {@inheritDoc}
-   * Runtime: O(n)
-   */
-  @Override
-  public String toString() {
-    if (isEmpty()) {
-      return "[]";
-    } else {
-      StringBuilder array = new StringBuilder(size).append("[");
-      for (int i = 0; i < size - 1; i++) {
-        array.append(arr[i] + ", ");
-      }
-      return array.append(arr[size - 1] + "]").toString();
+        return -1;
     }
-  }
+
+    /**
+     * {@inheritDoc} 
+     * Runtime: O(1)
+     */
+    @Override
+    public E get(int index) {
+        // check of index is within the appropriate range
+        if (index < 0 || index >= size) {
+            throw new IllegalArgumentException("ERROR: Illegal Index to get element: " + index);
+        }
+        return arr[index];
+    }
+
+    /**
+     * {@inheritDoc} 
+     * Runtime: O(1)
+     */
+    @Override
+    public void set(int index, E e) {
+        // check of index is within the appropriate range
+        if (index < 0 || index >= size) {
+            throw new IllegalArgumentException("ERROR: Illegal Index to set: " + index);
+        }
+        arr[index] = e;
+    }
+
+    /**
+     * {@inheritDoc} 
+     * When the size is at capacity, the array will resize double its 
+     * capacity 
+     * Runtime: Amortized O(1), Worst Case O(n)
+     */
+    @Override
+    public void add(E e) {
+        // double the length of the array if size is at capacity
+        if (size == arr.length) {
+            resize(2 * arr.length);
+        }
+        arr[size++] = e;
+    }
+
+    /**
+     * {@inheritDoc} 
+     * When the size is less than a quarter of capacity, the array
+     * capacity will resize down by half 
+     * Runtime: O(n)
+     */
+    @Override
+    public E remove(int index) {
+        if (isEmpty()) {
+            throw new NoSuchElementException("ERROR: can not remove from an empty array");
+        } else if (index < 0 || index >= size) {
+            throw new IllegalArgumentException("ERROR: Illegal Index to remove: " + index);
+        } else {
+            E prev = arr[index];
+            // shift the elements towards the left
+            for (int i = index; i < size; i++) {
+                arr[i] = arr[i + 1];
+            }
+            size--;
+            // resize the array by half is size less than a fourth of capacity
+            if (size < arr.length / 4) {
+                resize(arr.length / 2);
+            }
+            return prev;
+        }
+    }
+
+    /**
+     * {@inheritDoc} 
+     * Runtime: O(n)
+     */
+    @Override
+    public boolean remove(Object o) {
+        if (isEmpty()) {
+            throw new NoSuchElementException("ERROR: can not remove from an empty array");
+        }
+        if (o == null) {
+            for (int i = 0; i < size; i++) {
+                if (arr[i] == null) {
+                    remove(i);
+                    return true;
+                }
+            }
+        } else {
+            for (int i = 0; i < size; i++) {
+                if (arr[i].equals(o)) {
+                    remove(i);
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    /**
+     * This is a helper method that resizes the underlying generic dynamic array
+     * @param length the length of the array 
+     * Runtime: O(n)
+     */
+    private void resize(int length) {
+        E[] temp = (E[]) new Object[length];
+        for (int i = 0; i < size; i++) {
+            temp[i] = arr[i];
+        }
+        arr = temp;
+    }
+
+    /**
+     * {@inheritDoc}
+     * Runtime: O(1)
+     */
+    @Override
+    public void clear() {
+        arr = (E[]) new Object[2];
+        size = 0;
+    }
+
+    /**
+     * {@inheritDoc} 
+     * Runtime: O(n)
+     */
+    @Override
+    public Object[] toArray() {
+        Object[] array = new Object[size];
+        for (int i = 0; i < size; i++) {
+            array[i] = arr[i];
+        }
+        return array;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public Iterator<E> iterator() {
+        return new Iterator<E>() {
+            private int count = 0; // the counter of elements traveled so far
+            final private int expectedSize = size; // check for concurrent modification
+
+            /**
+             * {@inheritDoc} Checks for concurrent modification
+             * 
+             * @throws ConcurrentModificationException
+             */
+            @Override
+            public boolean hasNext() {
+                if (expectedSize != size) {
+                    throw new ConcurrentModificationException("ERROR: Cannot modifiy the iterator");
+                }
+                return count < expectedSize;
+            }
+
+            /**
+             * {@inheritDoc} Checks for concurrent modification
+             * 
+             * @throws ConcurrentModificationException
+             */
+            @Override
+            public E next() {
+                if (expectedSize != size) {
+                    throw new ConcurrentModificationException("ERROR: Cannot modifiy the iterator");
+                }
+                if (hasNext()) {
+                    return arr[count++];
+                } else {
+                    throw new NoSuchElementException("ERROR: No more elements to iterate");
+                }
+            }
+
+            /**
+             * {@inheritDoc}
+             */
+            @Override
+            public void remove() {
+                throw new UnsupportedOperationException("ERROR: Remove not Supported by this " + 
+                        "Iterator");
+            }
+        };
+    }
+
+    /**
+     * {@inheritDoc} 
+     * Runtime: O(n)
+     */
+    @Override
+    public String toString() {
+        if (isEmpty()) {
+            return "[]";
+        } else {
+            StringBuilder array = new StringBuilder(size).append("[");
+            for (int i = 0; i < size - 1; i++) {
+                array.append(arr[i] + ", ");
+            }
+            return array.append(arr[size - 1] + "]").toString();
+        }
+    }
 }
