3598ae03a680af9d94d1edf5d182baf4db5d4aac
Jason Tran <jasontran2022@gmail.com>
2020-08-20 21:27:58 -0400

    modified test file

--- src/test/priorityqueue/MaxPriorityQueueTest.java
+++ src/test/priorityqueue/MaxPriorityQueueTest.java
@@ -1,5 +1,30 @@
 package test.priorityqueue;
 
+import main.priorityqueue.*;
+import main.heap.Heap;
+import main.heap.BinaryMaxHeap;
+
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+import java.util.List;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.HashSet;
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+import java.util.ConcurrentModificationException;
+
 public class MaxPriorityQueueTest {
+    @Before
+    public void setUpMaxPriorityQueues() {
+        
+    }
     
+    @Test
+    public void testFirstConstructor() {
+        
+    }
 }
